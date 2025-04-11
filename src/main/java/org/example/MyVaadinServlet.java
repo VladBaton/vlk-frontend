c5156a17504408c37d5c427cc0d5dd6ac0f2fd2f
VladBaton <vladstalker2000@yandex.ru>
2025-04-11 17:59:23 +0300

    Initial commit

---
+++ src/main/java/org/example/MyVaadinServlet.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * This class is used to configure the generated html host page used by the app
+ */
+@PWA(name = "VLK", shortName = "VLK")
+public class MyVaadinServlet implements AppShellConfigurator {
+
+}
