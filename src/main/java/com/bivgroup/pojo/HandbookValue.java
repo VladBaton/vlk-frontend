adb3e2726ce0cf6ff7867b2ba72fef9924cac46b
VladBaton <vladstalker2000@yandex.ru>
2025-05-25 10:16:47 +0300

    CHSU-0008: копипаст пожи

---
+++ src/main/java/com/bivgroup/pojo/HandbookValue.java
@@ -0,0 +1,24 @@
+package com.bivgroup.pojo;
+
+public class HandbookValue {
+
+    private Object value;
+
+    private String description;
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
